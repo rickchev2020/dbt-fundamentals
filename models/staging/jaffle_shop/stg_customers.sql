59a6a16d8c0b87a0e39340106bc964df20bf0607
rick@chevaliergrp.com <rick@chevaliergrp.com>
2022-03-30 23:32:31 +0000

    source references

--- models/staging/jaffle_shop/stg_customers.sql
+++ models/staging/jaffle_shop/stg_customers.sql
@@ -3,6 +3,6 @@ select
     id as customer_id,
     first_name,
     last_name
-from {{ source('jaffle_shop', 'customers') }}
+from {{ source('jaffle_shop', 'customers')}}
 )
 select * from customers

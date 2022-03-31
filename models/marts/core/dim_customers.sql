6cded4cc08ff499d53a249b3bc6e86f79edfbeaa
Rick Chevalier <Rick@chevaliergrp.com>
2022-03-30 19:47:00 -0600

    Testing implementation

--- models/marts/core/dim_customers.sql
+++ models/marts/core/dim_customers.sql
@@ -1,11 +1,8 @@
-{{ config( materialized = 'table') }}
-
 with customers as (
-    select *
-    from {{ ref('stg_customers')}}
+    select * from {{ ref('stg_customers')}}
 ),
 orders as (
-    select * from {{ ref('stg_orders')}}
+    select * from {{ ref('fct_orders')}}
 ),
 customer_orders as (
     select
--- models/staging/jaffle_shop/stg_customers.sql
+++ models/staging/jaffle_shop/stg_customers.sql
@@ -1,8 +1,8 @@
 with customers as (
-select 
+select
     id as customer_id,
     first_name,
     last_name
-from {{ source('jaffle_shop', 'customers') }}
+from {{ source('jaffle_shop', 'customers')}}
 )
 select * from customers

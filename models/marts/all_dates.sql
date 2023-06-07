36ddc171a723226f6d2a30445b7f9c41f25ff5f6
Rick Chevalier <rick@chevaliergrp.com>
2023-06-07 19:21:12 +0000

    commit

--- models/marts/all_dates.sql
+++ models/marts/all_dates.sql
@@ -5,5 +5,5 @@
 {{ dbt_utils.date_spine(
      datepart="day",
     start_date="cast('2020-01-01' as date)",
-    end_date="cast('2021-01-01' as date)"
+    end_date="cast('2024-01-01' as date)"
 )}}

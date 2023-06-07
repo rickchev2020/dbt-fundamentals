d7930756a10e77be3987cca3fc0d4dbdd59bf3d7
Rick Chevalier <rick@chevaliergrp.com>
2023-06-07 19:20:48 +0000

    sync

--- models/marts/core/fct_orders.sql
+++ models/marts/core/fct_orders.sql
@@ -1,35 +1,27 @@
-with orders as (
+with
+    orders as (select * from {{ ref("stg_orders") }}),
 
-    select * from {{ ref('stg_orders') }}
+    payments as (select * from {{ ref("stg_payments") }}),
 
-),
+    order_payments as (
+        select order_id, sum(case when status = 'success' then amount end) as amount
 
-payments as (
+        from payments
+        group by 1
+    ),
 
-    select * from {{ ref('stg_payments') }}
+    final as (
 
-),
+        select
+            orders.order_id,
+            orders.customer_id,
+            orders.order_date,
+            coalesce(order_payments.amount, 0) as amount
 
-order_payments as (
-    select
-        order_id,
-        sum(case when status = 'success' then amount end) as amount
+        from orders
+        left join order_payments using (order_id)
 
-    from payments
-    group by 1
-),
+    )
 
-final as (
-
-    select
-        orders.order_id,
-        orders.customer_id,
-        orders.order_date,
-        coalesce(order_payments.amount, 0) as amount
-
-    from orders
-    left join order_payments using (order_id)
-
-)
-
-select * from final
+select *
+from final
